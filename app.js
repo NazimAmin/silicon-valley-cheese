6467ec3dd050b097ebc8b6dcf3e6a03320676a11
nazim <amin.nazim13@gmail.com>
2015-09-27 04:04:46 -0400

    lets pivot

--- app.js
+++ app.js
@@ -4,15 +4,23 @@
 var yosay = require('yosay');
 var chalk = require('chalk');
 var fs = require('fs');
+
 //parse & randomize quotes
 var data = JSON.parse(fs.readFileSync(__dirname + '/quotes.json'));
 var random_data = Math.floor(Math.random() * data.quotes.length);
-//randomize color
-var colors = ["green", "yellow", "magenta", "cyan", "white"];
+
+//randomize color index
+var colors = ["red", "green", "yellow", "magenta", "cyan", "white"];
 var random_color = Math.floor(Math.random() * colors.length);
+
+//apply color to text using chalk
 var passed = chalk.red(data.quotes[random_data]);
 
+//apply randomized color
 switch (colors[random_color]) {
+case "red":
+    passed = chalk.red(data.quotes[random_data]);
+    break;
 case "green":
     passed = chalk.green(data.quotes[random_data]);
     break;
